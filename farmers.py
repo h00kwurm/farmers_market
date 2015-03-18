6e5ab34999c1b8f322c69e0322f2c8f230bd5190
Aditya Natraj <adityajones@gmail.com>
2015-03-18 17:45:51 -0400

    PyGal. It's cool.
    creates a pip3 dependency on pygal but now we can
    make graphs. a simple graph rendered as svg is in
    the graphs folder. i think this is a reasonable
    way to keep it.

--- farmers.py
+++ farmers.py
@@ -2,6 +2,7 @@
 
 import csv
 import itertools
+import pygal
 from os import listdir
 from os.path import isfile, join, dirname
 
@@ -59,15 +60,20 @@ def do_they_all_have_xy(markets):
   else:
     return xyless
 
+def plot_percentages_of_items(percentages, outputfile):
+  bar_chart = pygal.Bar(style=pygal.style.LightSolarizedStyle, show_legend=False, x_label_rotation=20)
+  bar_chart.title = "What chance do I have of finding _ at my Farmer's market?"
+  bar_chart.x_labels = [name for (name, percent) in percentages]
+  bar_chart.add('', [round(100*percent, 1) for (name, percent) in percentages])
+  bar_chart.render_to_file(outputfile)
+
 def percentages_of_items(items, markets):
   count = [0]*len(items)
   for market in markets:
     for index, item in enumerate(items):
       if market.get(item) == 'Y':
         count[index] += 1
-  print("total markets: ", len(markets))
-  for index, item in enumerate(items):
-    print(item, count[index]/len(markets)) 
+  return [(items[index], count[index]/len(markets)) for index in range(len(items))]
 
 def average_number_of_items(items, markets):
   item_count_average = 0
@@ -77,10 +83,13 @@ def average_number_of_items(items, markets):
       if market.get(item) == 'Y':
         current_item_count += 1
     item_count_average = (item_count_average * market_index + current_item_count) / (market_index + 1)
-  print("average number of items: ", item_count_average)
+  print('average number of items: ', item_count_average)
 
-def pair_array_add(array_one, array_two):
-  return [array_one[0] + array_two[0], array_one[1] + array_two[1]]
+def array_add(array_one, array_two):
+  if len(array_one) != len(array_two):
+    raise Exception('invalid sizing')
+  output = []*len(array_one)
+  return [(array_one[index] + array_two[index]) for index in range(len(array_one))]
 
 def pair_map_to_probability(pair):
   return (pair[0] / float(pair[1]))
@@ -95,21 +104,34 @@ def generate_pairwise_conditional_probabilities(items, markets):
         if market.get(item_b) == 'N':
           continue
         if market.get(item_b) == 'Y' and market.get(item_a) == 'Y':
-          probability_map[(item_a, item_b)] = pair_array_add(probability_map[(item_a, item_b)], [1, 1])
+          probability_map[(item_a, item_b)] = array_add(probability_map[(item_a, item_b)], [1, 1])
         else:
-          probability_map[(item_a, item_b)] = pair_array_add(probability_map[(item_a, item_b)], [0, 1])
+          probability_map[(item_a, item_b)] = array_add(probability_map[(item_a, item_b)], [0, 1])
       probability_map[(item_a, item_b)] = pair_map_to_probability(probability_map[(item_a, item_b)])
   return probability_map
+
+# i was curious about independence
+# max(P(A|B), P(B|A))
+# again, could be faster because duplicates dist(x, y) == dist(y, x)
+# i'll choose memory over speed here (dont need to keep 'seen' list)
+# or i could just regenerate all the combinations using itertools...
+def find_largest_disparity(probabilities):
+  max_disparity = ["", "", 0]
+  for (item_a, item_b) in probabilities.keys():
+    distance = abs(probabilities[(item_a, item_b)] - probabilities[(item_b, item_a)])
+    if distance > max_disparity[2]:
+      max_disparity = [item_a, item_b, distance]
+  return max_disparity
 
 def main():
 
   data_file_path = find_local_datafile()
   if data_file_path is None:
-    return fail("couldnt find the data file in this directory")
+    return fail('couldnt find the data file in this directory')
 
   markets, legend = parse_datafile(data_file_path)
   if len(markets) == 0:
-    return fail("found no markets in datafile")
+    return fail('found no markets in datafile')
 
   xyless = do_they_all_have_xy(markets)
   if xyless is not None:
@@ -117,10 +139,16 @@ def main():
 
   items = ['bakedgoods', 'cheese', 'crafts', 'flowers', 'eggs', 'seafood', 'herbs', 'vegetables', 'honey', 'jams', 'maple', 'meat', 'nursery', 'nuts', 'plants', 'poultry', 'prepared', 'soap', 'trees', 'wine']
 
-  average_number_of_items(items, markets)
-  percentages_of_items(items, markets)
+  # average_number_of_items(items, markets)
+  item_percents = percentages_of_items(items, markets)
+  plot_percentages_of_items(item_percents, 'graphs/percentages.svg')
+  
   # probabilities = generate_pairwise_conditional_probabilities(items, markets)
-
+  
+  # disparity = find_largest_disparity(probabilities)
+  # print("max disparity between P(a|b) and P(b|a)")
+  # print(disparity)
+  # print(probabilities[(disparity[0], disparity[1])], probabilities[(disparity[1], disparity[0])])
   return EXIT_SUCCESS
 
 if __name__ == '__main__':
